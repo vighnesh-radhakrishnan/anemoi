b88fc89cf87b474afb3e0c9df5c66a6b8bbf33d3
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2025-02-03 12:22:31 +0530

    footer added to each page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Constructors from "./Components/Constructors";
 import Drivers from "./Components/Drivers";
 import TrackDominance from "./Components/TrackDominance";
 import { PageWrapper, TabWrapper, Tab } from "./Components/Container";
+import Footer from "./Components/Footer";
 
 function App() {
   const [activeTab, setActiveTab] = useState("calendar");
@@ -17,71 +18,74 @@ function App() {
   };
 
   return (
-    <PageWrapper>
-      <header className="App-header">
-        <TabWrapper>
-          <Tab
-            active={activeTab === "calendar"}
-            onClick={() => handleTabChange("calendar")}
-          >
-            Calendar
-          </Tab>
-          <Tab
-            active={activeTab === "standings"}
-            onClick={() => handleTabChange("standings")}
-          >
-            Standings
-          </Tab>
-          <Tab
-            active={activeTab === "session"}
-            onClick={() => handleTabChange("session")}
-          >
-            Session Details
-          </Tab>
-          <Tab
-            active={activeTab === "fastestLap"}
-            onClick={() => handleTabChange("fastestLap")}
-          >
-            Fastest Lap
-          </Tab>
-          <Tab
-            active={activeTab === "trackdominance"}
-            onClick={() => handleTabChange("trackdominance")}
-          >
-            Track Dominance
-          </Tab>
-          <Tab
-            active={activeTab === "circuits"}
-            onClick={() => handleTabChange("circuits")}
-          >
-            Circuits
-          </Tab>
-          <Tab
-            active={activeTab === "constructors"}
-            onClick={() => handleTabChange("constructors")}
-          >
-            Constructors
-          </Tab>
-          <Tab
-            active={activeTab === "drivers"}
-            onClick={() => handleTabChange("drivers")}
-          >
-            Drivers
-          </Tab>
-        </TabWrapper>
+    <>
+      <PageWrapper>
+        <header className="App-header">
+          <TabWrapper>
+            <Tab
+              active={activeTab === "calendar"}
+              onClick={() => handleTabChange("calendar")}
+            >
+              Calendar
+            </Tab>
+            <Tab
+              active={activeTab === "standings"}
+              onClick={() => handleTabChange("standings")}
+            >
+              Standings
+            </Tab>
+            <Tab
+              active={activeTab === "session"}
+              onClick={() => handleTabChange("session")}
+            >
+              Session Details
+            </Tab>
+            <Tab
+              active={activeTab === "fastestLap"}
+              onClick={() => handleTabChange("fastestLap")}
+            >
+              Fastest Lap
+            </Tab>
+            <Tab
+              active={activeTab === "trackdominance"}
+              onClick={() => handleTabChange("trackdominance")}
+            >
+              Track Dominance
+            </Tab>
+            <Tab
+              active={activeTab === "circuits"}
+              onClick={() => handleTabChange("circuits")}
+            >
+              Circuits
+            </Tab>
+            <Tab
+              active={activeTab === "constructors"}
+              onClick={() => handleTabChange("constructors")}
+            >
+              Constructors
+            </Tab>
+            <Tab
+              active={activeTab === "drivers"}
+              onClick={() => handleTabChange("drivers")}
+            >
+              Drivers
+            </Tab>
+          </TabWrapper>
 
-        <>
-          {activeTab === "session" && <Session />}
-          {activeTab === "calendar" && <Calendar />}
-          {activeTab === "fastestLap" && <FastestLap />}
-          {activeTab === "trackdominance" && <TrackDominance />}
-          {activeTab === "circuits" && <Circuits />}
-          {activeTab === "standings" && <Standings />}
-          {activeTab === "constructors" && <Constructors />}
-          {activeTab === "drivers" && <Drivers />}
-        </>
-      </header>
-    </PageWrapper>
+          <>
+            {activeTab === "session" && <Session />}
+            {activeTab === "calendar" && <Calendar />}
+            {activeTab === "fastestLap" && <FastestLap />}
+            {activeTab === "trackdominance" && <TrackDominance />}
+            {activeTab === "circuits" && <Circuits />}
+            {activeTab === "standings" && <Standings />}
+            {activeTab === "constructors" && <Constructors />}
+            {activeTab === "drivers" && <Drivers />}
+          </>
+        </header>
+      </PageWrapper>
+      <Footer />
+    </>
   );
 }
 

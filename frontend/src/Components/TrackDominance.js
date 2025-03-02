51abf325cc9613a75982fad8943b307a497ce0ba
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2025-03-02 19:18:52 +0530

    import race data form and replace current form code from tack dominance

--- frontend/src/Components/TrackDominance.js
+++ frontend/src/Components/TrackDominance.js
@@ -1,3 +1,4 @@
+// src/pages/TrackDominance.jsx
 // @ts-nocheck
 import React, { useState } from "react";
 import axios from "axios";
@@ -8,6 +9,7 @@ import {
   NoDataMessage,
   SessionDetails,
 } from "./Container";
+import RaceDataForm from "../components/RaceDataForm";
 
 const TrackDominance = () => {
   const [selectedYear, setSelectedYear] = useState("");
@@ -19,19 +21,11 @@ const TrackDominance = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const handleYearChange = (event) => setSelectedYear(event.target.value);
-  const handleGrandPrixChange = (event) => setGrandPrix(event.target.value);
-  const handleIdentifierChange = (event) =>
-    setSessionIdentifier(event.target.value);
-  const handleDriver1Change = (event) => setDriver1(event.target.value);
-  const handleDriver2Change = (event) => setDriver2(event.target.value);
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
     setError(false);
     setDominanceData(null);
-
     try {
       const response = await axios.get(
         `https://anemoi-backend.onrender.com/track-dominance?year=${selectedYear}&gp=${grandPrix}&identifier=${sessionIdentifier}&driver1=${driver1}&driver2=${driver2}`
@@ -55,51 +49,21 @@ const TrackDominance = () => {
   return (
     <PageWrapper>
       <h1>Track Dominance</h1>
-
-      <FormWrapper
-        onSubmit={handleSubmit}
-        onInput={() => {
-          setError(false); // Clear the error message when user modifies input
-        }}
-      >
-        <input
-          type="number"
-          placeholder="Enter Year"
-          value={selectedYear}
-          onChange={handleYearChange}
-          required
+      <FormWrapper onInput={() => setError(false)}>
+        <RaceDataForm
+          selectedYear={selectedYear}
+          setSelectedYear={setSelectedYear}
+          grandPrix={grandPrix}
+          setGrandPrix={setGrandPrix}
+          sessionIdentifier={sessionIdentifier}
+          setSessionIdentifier={setSessionIdentifier}
+          driver1={driver1}
+          setDriver1={setDriver1}
+          driver2={driver2}
+          setDriver2={setDriver2}
+          handleSubmit={handleSubmit}
         />
-        <input
-          type="text"
-          placeholder="Enter Grand Prix Name"
-          value={grandPrix}
-          onChange={handleGrandPrixChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Enter Session Identifier"
-          value={sessionIdentifier}
-          onChange={handleIdentifierChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Enter Driver 1 (e.g, HAM)"
-          value={driver1}
-          onChange={handleDriver1Change}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Enter Driver 2 (e.g, VER)"
-          value={driver2}
-          onChange={handleDriver2Change}
-          required
-        />
-        <button type="submit">Get Plot</button>
       </FormWrapper>
-
       {loading && (
         <div
           style={{
@@ -111,7 +75,6 @@ const TrackDominance = () => {
           <img src={LoadingGif} alt="Loading..." width="150" height="150" />
         </div>
       )}
-
       {dominanceData && !loading && (
         <div>
           <SessionDetails>
@@ -136,7 +99,6 @@ const TrackDominance = () => {
               <span>{dominanceData.driver2}</span>
             </div>
           </SessionDetails>
-
           {dominanceData.image_base64 && (
             <div>
               <h2 className="image-header">
@@ -154,7 +116,6 @@ const TrackDominance = () => {
           )}
         </div>
       )}
-
       {error && !dominanceData && !loading && (
         <NoDataMessage>
           Sorry, no track dominance details found for the provided input. Please
